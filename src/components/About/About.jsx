6aaf7273c272d779820a40592aa754b95eff3119
Tanu Panwar <tanupanwar2306@gmail.com>
2025-08-15 14:27:34 +0530

    Update About.jsx
    Updated resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1scizo0RR_FUIaebmzEbulUTnVGbR-vip/view?usp=sharing"
+            href="https://drive.google.com/file/d/1J3QuSJ2YQWkHpLcvAxGNLLOsWjeJFhr8/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
